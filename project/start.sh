1a36a8eebb291ca8333cd0ae1d28a10586b18944
Agil-Laptop Acer Swift 3 <muhmdagil95@gmail.com>
2022-10-06 07:59:21 +0700

    litle fix start.sh docker

--- project/start.sh
+++ project/start.sh
@@ -5,4 +5,4 @@ export FLASK_APP=project
 # export FLASK_RUN_HOST=0.0.0.0
 # export FLASK_RUN_PORT=5000
 # flask run --with-threads
-gunicorn --conf project/gunicorn_conf.py --bind 0.0.0.0:5000 project:create_app
+gunicorn --conf project/gunicorn_conf.py --bind 0.0.0.0:5000 "project:create_app()"
